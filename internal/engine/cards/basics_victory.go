34e0e1ba15609bc6bdadf82d257d7c53356fc195
Nutthawit Lilawannasin <nutthawit.l@proton.me>
2026-04-14 01:14:00 +0000

    feat(engine): add Estate, Duchy, Province, Curse

---
+++ internal/engine/cards/basics_victory.go
@@ -0,0 +1,42 @@
+package cards
+
+import "github.com/tie/dominion-grpc/internal/engine"
+
+var Estate = &engine.Card{
+	ID:            "estate",
+	Name:          "Estate",
+	Cost:          2,
+	Types:         []engine.CardType{engine.TypeVictory},
+	VictoryPoints: func(engine.PlayerState) int { return 1 },
+}
+
+var Duchy = &engine.Card{
+	ID:            "duchy",
+	Name:          "Duchy",
+	Cost:          5,
+	Types:         []engine.CardType{engine.TypeVictory},
+	VictoryPoints: func(engine.PlayerState) int { return 3 },
+}
+
+var Province = &engine.Card{
+	ID:            "province",
+	Name:          "Province",
+	Cost:          8,
+	Types:         []engine.CardType{engine.TypeVictory},
+	VictoryPoints: func(engine.PlayerState) int { return 6 },
+}
+
+var Curse = &engine.Card{
+	ID:            "curse",
+	Name:          "Curse",
+	Cost:          0,
+	Types:         []engine.CardType{engine.TypeCurse},
+	VictoryPoints: func(engine.PlayerState) int { return -1 },
+}
+
+func init() {
+	DefaultRegistry.Register(Estate)
+	DefaultRegistry.Register(Duchy)
+	DefaultRegistry.Register(Province)
+	DefaultRegistry.Register(Curse)
+}
---
+++ internal/engine/cards/basics_victory_test.go
@@ -0,0 +1,37 @@
+package cards
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tie/dominion-grpc/internal/engine"
+)
+
+func TestEstate_VictoryPoints(t *testing.T) {
+	require.Equal(t, 1, Estate.VictoryPoints(engine.PlayerState{}))
+	require.Equal(t, 2, Estate.Cost)
+	require.True(t, Estate.HasType(engine.TypeVictory))
+}
+
+func TestDuchy_VictoryPoints(t *testing.T) {
+	require.Equal(t, 3, Duchy.VictoryPoints(engine.PlayerState{}))
+	require.Equal(t, 5, Duchy.Cost)
+}
+
+func TestProvince_VictoryPoints(t *testing.T) {
+	require.Equal(t, 6, Province.VictoryPoints(engine.PlayerState{}))
+	require.Equal(t, 8, Province.Cost)
+}
+
+func TestCurse_NegativeVP(t *testing.T) {
+	require.Equal(t, -1, Curse.VictoryPoints(engine.PlayerState{}))
+	require.Equal(t, 0, Curse.Cost)
+	require.True(t, Curse.HasType(engine.TypeCurse))
+}
+
+func TestVictories_HaveNoOnPlay(t *testing.T) {
+	require.Nil(t, Estate.OnPlay)
+	require.Nil(t, Duchy.OnPlay)
+	require.Nil(t, Province.OnPlay)
+	require.Nil(t, Curse.OnPlay)
+}
